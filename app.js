71c7ba72c8df74ef69a44067772a0b20e08cdc3c
wongsitu <sa_waika_4d_23@hotmail.com>
2018-08-12 01:00:17 -0700

    Added prices of products in JS

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
 //Create template
-function Products(image,name,type){
+function Products(image,name,price){
     this.image = image;
     this.name = name;
-    this.type = type;
+    this.price = price;
 }
 //Cereals
-var cherrios = new Products("./solution-code/images/cereal/cheerios.jpg","Cheerios","cereal");
-var crunch = new Products("./solution-code/images/cereal/crunch.jpg","Crunch","cereal");
-var fruit_loops = new Products("./solution-code/images/cereal/fruit-loops.jpg","Fruit-loops","cereal");
-var puffs = new Products("./solution-code/images/cereal/puffs.jpg","Puffs","cereal");
-var raisin_brand = new Products("./solution-code/images/cereal/raisin-bran.jpg","Raisin Brand","cereal");
+var cherrios = new Products("./solution-code/images/cereal/cheerios.jpg","Cheerios",2.79);
+var crunch = new Products("./solution-code/images/cereal/crunch.jpg","Crunch",3.39);
+var fruit_loops = new Products("./solution-code/images/cereal/fruit-loops.jpg","Fruit-loops",3.64);
+var puffs = new Products("./solution-code/images/cereal/puffs.jpg","Puffs",2.99);
+var raisin_brand = new Products("./solution-code/images/cereal/raisin-bran.jpg","Raisin Brand",2.69);
 
 var cereal_array = [cherrios, crunch, fruit_loops, puffs, raisin_brand];
 var cereal_btn = document.getElementById("cereal");
 
 //Candy
-var kit_kat = new Products("./solution-code/images/candy/kit-kat.jpg","Kit-kat","candy");
-var mms = new Products("./solution-code/images/candy/m&ms.jpg","M&ms","candy");
-var skittles = new Products("./solution-code/images/candy/skittles.jpg","Skittles","candy");
-var snickers = new Products("./solution-code/images/candy/snickers.jpg","Snickers","candy");
-var twix = new Products("./solution-code/images/candy/twix.jpg","Twix","candy");
+var kit_kat = new Products("./solution-code/images/candy/kit-kat.jpg","Kit-kat",1.49);
+var mms = new Products("./solution-code/images/candy/m&ms.jpg","M&ms",1.39);
+var skittles = new Products("./solution-code/images/candy/skittles.jpg","Skittles",1.49);
+var snickers = new Products("./solution-code/images/candy/snickers.jpg","Snickers",1.25);
+var twix = new Products("./solution-code/images/candy/twix.jpg","Twix",0.99);
 
 var candy_array = [kit_kat, mms, skittles, snickers, twix];
 var candy_btn = document.getElementById("candy");
 
 //Juice
-var apple = new Products("./solution-code/images/juice/apple-juice.jpg","Apple juice","juice");
-var carrot = new Products("./solution-code/images/juice/carrot-juice.jpg","Carrot juice","juice");
-var grape = new Products("./solution-code/images/juice/grape-juice.jpeg","Grape juice","juice");
-var orange = new Products("./solution-code/images/juice/orange-juice.jpg","Orange juice","juice");
-var tomato = new Products("./solution-code/images/juice/tomato-juice.jpg","Tomato juice","juice");
+var apple = new Products("./solution-code/images/juice/apple-juice.jpg","Apple juice",4.81);
+var carrot = new Products("./solution-code/images/juice/carrot-juice.jpg","Carrot juice",3.43);
+var grape = new Products("./solution-code/images/juice/grape-juice.jpeg","Grape juice",4.98);
+var orange = new Products("./solution-code/images/juice/orange-juice.jpg","Orange juice",3.64);
+var tomato = new Products("./solution-code/images/juice/tomato-juice.jpg","Tomato juice",2.87);
 
 var juice_array = [apple, carrot, grape, orange, tomato];
 var juices_btn = document.getElementById("juices");
@@ -62,10 +62,10 @@ function displayProducts(prodArray){
         //attributes and CSS
         anchor.className =`${i}`;
         anchor.href="#";
-        anchor.setAttribute("style","border: 1px solid grey; display: inline-block;");
+        anchor.setAttribute("style","border: 2px solid grey; display: inline-block;");
         img.setAttribute("src",prodArray[i].image);
         img.setAttribute("style","width: 94%; margin: 3%;");
-        nameHolder.innerText = `${prodArray[i].name}`;
+        nameHolder.innerText = `${prodArray[i].name}  $${prodArray[i].price}`;
         nameHolder.setAttribute("style","margin: 0;");
         //display
         anchor.appendChild(img);
@@ -85,11 +85,13 @@ function displayCart(prodArray){
     //Reloads the updated shoppingCart list
     for(var k = 0; k < prodArray.length; k++){
         var prod_name = prodArray[k].name;
+        var prod_price = prodArray[k].price;
         var element = document.createElement("li");
         element.className = `name-${k}`
-        element.innerText = `${prod_name}`;
+        element.innerText = `${prod_name} $${prod_price}`;
         list.appendChild(element)
     }
+    
     // console.log(shoppingCart); debbuger print
 }
 
@@ -111,6 +113,14 @@ function selection(){
             toShoppingCart(productnumber);
         });
     });
+}
+
+function CalculateTotal(prices_array){
+    var TotalPrice = 0
+    for( var i = 0; i < prices_array.length; i++){
+        TotalPrice = TotalPrice + prices_array[i].price[i];
+    }
+    return TotalPrice;
 }
 
 //EventListeners
